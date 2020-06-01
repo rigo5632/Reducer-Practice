699289a9faba7e27039cfef666b14ae92a61e6cd
rigo5632 <rmquiroz5@gmail.com>
2020-06-01 07:32:23 -0600

    Updates

---
+++ precache-manifest.1086ada47c03a3d53e64f5e077697198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0bde0a41282925d4ae44eca1748300c",
+    "url": "/web-app/index.html"
+  },
+  {
+    "revision": "23ecd8e5a8a0f5d8bba8",
+    "url": "/web-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f254a28e806e89875da5",
+    "url": "/web-app/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "23ecd8e5a8a0f5d8bba8",
+    "url": "/web-app/static/js/2.06b2c078.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-app/static/js/2.06b2c078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f254a28e806e89875da5",
+    "url": "/web-app/static/js/main.3f6a367b.chunk.js"
+  },
+  {
+    "revision": "b2b0e22eec4cebc65499",
+    "url": "/web-app/static/js/runtime-main.495e4c17.js"
+  }
+]);
--- src/reducers/TabReducer.js
+++
@@ -1,9 +0,0 @@
-export const TabReducer = (state, action) =>{
-    switch(action.type){
-        case 'SWITCH_DISPLAY':
-            state.map(tab => action.counter++ === action.id ? tab.display = !tab.display : tab.display = false);
-            return state;
-        default:
-            return state;
-    }
-}
